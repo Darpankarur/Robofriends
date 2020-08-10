85f8e5fe47fd721246a0721fb989ee6bdcf8b980
Darpankarur <darpannk@gmail.com>
2020-08-10 20:16:22 +0530

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# Robofriends
-Created a simple user search app using basics of create-react-app.
---
+++ precache-manifest.dd2da8cc84afe78f8e88d3d6404c41d0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee84cb13df856a427b92b810b5513fda",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/Robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "02fdd8792a70b2d409f6",
+    "url": "/Robofriends/static/css/main.19276c36.chunk.css"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/Robofriends/static/js/2.f690e5d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robofriends/static/js/2.f690e5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02fdd8792a70b2d409f6",
+    "url": "/Robofriends/static/js/main.e6d0188f.chunk.js"
+  },
+  {
+    "revision": "a6103a62c81f6271b765",
+    "url": "/Robofriends/static/js/runtime-main.79cc7977.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
